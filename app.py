dd925b7fcc3bfe0d8c70250c2224e80d4eb56afe
FilippKramer <krame490@gmail.com>
2019-01-11 13:41:25 -0800

    Add static files

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 @app.route("/")
 def hello(): 
-    return "Start of Flask App"
+  url_for('static', filename='style.css')
+  return "Change occurred"
+  
+
+@app.route("/hello")
+def what():
+  return "WE are in hello"
+
+#converter
+# string, int, float, path - (like string but accepts slash), uuid
+
+@app.route('/post/<string:post_id>')
+def noError(post_id):
+  return "%s" % post_id
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         return do_the_login()
+#     else:
+#         return show_the_login_form()
